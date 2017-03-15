20f6c47da6068818a495c582d3a0084f3a072719
David Zuluaga <davidzuluagarios@gmail.com>
2017-03-15 11:37:42 -0500

    Cambio de configuraciones del componente de seleccion franja
    Se hace el cambio de la configuracion del componente para que este sea parametrizado desde el portal, se hace la validacion del tiempo para programar la llamada

--- app/src/main/java/co/com/une/appmovilesune/components/CustomTimePickerDialog.java
+++ app/src/main/java/co/com/une/appmovilesune/components/CustomTimePickerDialog.java
@@ -6,11 +6,17 @@ import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.NumberPicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import co.com.une.appmovilesune.MainActivity;
+import co.com.une.appmovilesune.R;
 
 /**
  * Created by davidzuluaga on 22/02/17.
@@ -30,17 +36,29 @@ public class CustomTimePickerDialog extends TimePickerDialog {
     int hour;
     int minute;
 
+    private int hourMin;
+    private int hourMax;
+    private int interval;
+    private int tiempoLlamada;
+
     public CustomTimePickerDialog(Context context, OnTimeSetListener listener,
                                   int hourOfDay, int minute, boolean is24HourView) {
         super(context, TimePickerDialog.THEME_HOLO_LIGHT, null, hourOfDay,
                 minute / TIME_PICKER_INTERVAL, is24HourView);
+
+        hourMin = Integer.parseInt(MainActivity.basedatos.consultar(false,"listasvalores",new String[] {"lst_valor"}, "lst_nombre = ? and lst_clave = ?",new String[] {"configuracionSelectorFranja", "horaMinima"}, null, null, null).get(0).get(0));
+        hourMax = Integer.parseInt(MainActivity.basedatos.consultar(false,"listasvalores",new String[] {"lst_valor"}, "lst_nombre = ? and lst_clave = ?",new String[] {"configuracionSelectorFranja", "horaMaxima"}, null, null, null).get(0).get(0));
+        interval = Integer.parseInt(MainActivity.basedatos.consultar(false,"listasvalores",new String[] {"lst_valor"}, "lst_nombre = ? and lst_clave = ?",new String[] {"configuracionSelectorFranja", "intervalo"}, null, null, null).get(0).get(0));
+        tiempoLlamada = Integer.parseInt(MainActivity.basedatos.consultar(false,"listasvalores",new String[] {"lst_valor"}, "lst_nombre = ? and lst_clave = ?",new String[] {"configuracionSelectorFranja", "tiempoLlamada"}, null, null, null).get(0).get(0));
+
         mTimeSetListener = listener;
+
     }
 
     @Override
     public void updateTime(int hourOfDay, int minuteOfHour) {
         mTimePicker.setCurrentHour(hourOfDay);
-        mTimePicker.setCurrentMinute(minuteOfHour / TIME_PICKER_INTERVAL);
+        mTimePicker.setCurrentMinute(minuteOfHour / interval);
     }
 
     @Override
@@ -48,14 +66,41 @@ public class CustomTimePickerDialog extends TimePickerDialog {
         switch (which) {
             case BUTTON_POSITIVE:
                 if (mTimeSetListener != null) {
-                    mTimeSetListener.onTimeSet(mTimePicker, mTimePicker.getCurrentHour(),
-                            mTimePicker.getCurrentMinute() * TIME_PICKER_INTERVAL);
+                    Log.d("Tiempo", String.valueOf(mTimePicker.getCurrentHour()));
+                    Log.d("Tiempo", String.valueOf(mTimePicker.getCurrentMinute() * interval));
+
+                    if(esTiempoTranscurridoCorrecto(mTimePicker.getCurrentHour(),mTimePicker.getCurrentMinute()*interval)){
+                        mTimeSetListener.onTimeSet(mTimePicker, mTimePicker.getCurrentHour(),
+                                mTimePicker.getCurrentMinute() * interval);
+                    }else{
+                        Toast.makeText(getContext(),getContext().getResources().getString(R.string.mensajefranjainvalidaa)+" "+tiempoLlamada+" "+getContext().getResources().getString(R.string.mensajefranjainvalidab),Toast.LENGTH_LONG).show();
+                        cancel();
+                    }
+
                 }
                 break;
             case BUTTON_NEGATIVE:
                 cancel();
                 break;
         }
+    }
+
+    private boolean esTiempoTranscurridoCorrecto(int horaSeleccionada, int minutoSeleccionado){
+        boolean valido = true;
+
+        Date horaActual= new Date(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime()));
+        Date horaProgramada = new Date(new SimpleDateFormat("yyyy/MM/dd ").format(Calendar.getInstance().getTime()) + String.format("%02d", horaSeleccionada)+":"+String.format("%02d", minutoSeleccionado)+":00");
+
+        Log.d("horaActual",horaActual.toString()+" "+String.valueOf(horaActual.getTime()));
+        Log.d("horaProgramada",horaProgramada.toString()+" "+String.valueOf(horaProgramada.getTime()));
+
+        long diferencia = ((horaProgramada.getTime() - horaActual.getTime())/1000)/60;
+
+        if(diferencia < tiempoLlamada){
+            valido = false;
+        }
+
+        return valido;
     }
 
     @Override
@@ -91,9 +136,9 @@ public class CustomTimePickerDialog extends TimePickerDialog {
     private void habilitarFranjas(int minimo){
         Log.d("Minimo ",""+minimo+"");
         minuteSpinner.setMinValue(0);
-        minuteSpinner.setMaxValue((60 / TIME_PICKER_INTERVAL) - 1);
+        minuteSpinner.setMaxValue((60 / interval) - 1);
         List<String> displayedValues = new ArrayList<>();
-        for (int i = minimo; i < 60; i += TIME_PICKER_INTERVAL) {
+        for (int i = minimo; i < 60; i += interval) {
             displayedValues.add(String.format("%02d", i));
         }
         Log.d("displayedValues.size()",String.valueOf(displayedValues.size()));
@@ -116,15 +161,11 @@ public class CustomTimePickerDialog extends TimePickerDialog {
 
     private void calcularHorarios(){
 
-        if(hour >= HOUR_MIN && hour <= HOUR_MAX){
+        if(hour >= hourMin && hour <= hourMax){
             hourSpinner.setMinValue(hour);
-            hourSpinner.setMaxValue(HOUR_MAX);
+            hourSpinner.setMaxValue(hourMax);
 
-            if(hour == hourSpinner.getValue()){
-                habilitarFranjas(0);
-            }
-
-            //habilitarFranjas(0);
+            habilitarFranjas(0);
 
         }else{
             deshabilitarHorario();
@@ -137,7 +178,7 @@ public class CustomTimePickerDialog extends TimePickerDialog {
         public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
             Log.d("hourChange", oldVal + " to " + newVal);
             //calcularHorarios();
-            if (hourSpinner.getValue() == HOUR_MAX) {
+            if (hourSpinner.getValue() == hourMax) {
                 deshabilitarFranjas();
             } else if (hourSpinner.getValue() == hour){
                 habilitarFranjas(0);
